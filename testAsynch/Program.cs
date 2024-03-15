88d30df6c3b7341d70884920e098bf943200ddf1
Charlie Brown <CBROWN@brock.ac.uk>
2024-03-15 14:12:48 +0000

    Add project files.

---
+++ testAsynch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+
+namespace testAsynch
+{
+    class Program
+    {
+        static Task<string> TaskGetStudents()
+        {
+
+            return Task.Run<string>(() =>
+           {
+               clsDBConnector connector = new clsDBConnector();
+               OleDbDataReader dr;
+               connector.Connect();
+               string sqlStr = "select * from tblStudent";
+               dr = connector.DoSQL(sqlStr);
+               while (dr.Read())
+               {
+                   Console.WriteLine($"{dr[0].ToString()}   {dr[1].ToString()}   {dr[2].ToString()} ");
+               }
+               return "Y";
+           });
+        }
+
+        static async void TestMethodAsync()
+        {
+            string result = await TaskGetStudents();
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("A");
+            TestMethodAsync();
+            Console.WriteLine("B");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ testAsynch/clsDBConnector.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Data.OleDb;
+
+namespace testAsynch
+{
+    class clsDBConnector
+    {
+        OleDbConnection conn = new OleDbConnection();
+        string dbProvider;
+        string dbSource;
+
+        public void Connect()
+        {
+            dbProvider = "Provider=Microsoft.ACE.OLEDB.12.0;";
+            dbSource = @"Data Source =C:\Users\charl\OneDrive - Brockenhurst College\Visual Studio\dbStudentlates.accdb";
+            conn.ConnectionString = dbProvider + dbSource;
+           
+            conn.Open();
+        }
+
+        public void close()
+        {
+            conn.Close();
+        }
+
+        public void DoDML(string dmlString)
+        {
+            OleDbCommand cmd;
+            cmd = new OleDbCommand(dmlString, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        public OleDbDataReader DoSQL(string sqlString)
+        {
+            OleDbCommand cmd;
+            cmd = new OleDbCommand(sqlString, conn);
+            return cmd.ExecuteReader() ;
+        }  
+    }
+}
